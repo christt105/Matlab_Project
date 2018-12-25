4b566b96d7359a293e85cb825682797482aa5de7
Christian Martínez <christt105@gmail.com>
2018-12-25 13:52:42 +0100

    Solved quat2rotmat error

--- quat2RotMat.m
+++ quat2RotMat.m
@@ -2,8 +2,7 @@ function [R] = quat2RotMat(q)
 %QUAT2ROTMAT Calculates rotation matrix of a quaternion
 %   Detailed explanation goes here
 
-R = [q(1)^2+q(2)^2-q(3)^2-q(4)^2, 2*q(2)*q(4)-2*q(1)*q(4), 2*q(2)*q(4)+2*q(1)*q(3);
+R = [q(1)^2+q(2)^2-q(3)^2-q(4)^2, 2*q(2)*q(3)-2*q(1)*q(4), 2*q(2)*q(4)+2*q(1)*q(3);
     2*q(2)*q(3)+2*q(1)*q(4),    q(1)^2-q(2)^2+q(3)^2-q(4)^2, 2*q(3)*q(4)-2*q(1)*q(2);
     2*q(2)*q(4)-2*q(1)*q(3),    2*q(3)*q(4)+2*q(1)*q(2),    q(1)^2-q(2)^2-q(3)^2+q(4)^2];
 end
-
