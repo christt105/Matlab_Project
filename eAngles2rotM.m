294cd717eb4987e1f799da529c80080858966690
Christian Martínez <christt105@gmail.com>
2018-12-29 22:34:59 +0100

    dummy commit

--- eAngles2rotM.m
+++ eAngles2rotM.m
@@ -1,4 +1,4 @@
-function rotM =  eAngles2rotM(phi, theta, psy)
+function [rotM] =  eAngles2rotM(phi, theta, psy)
 
 %EANGLES2ROTM Given an euler angles(degrees), it returns its rotation matrix
 %   ATTENTION: euler angles must be in degrees!
@@ -31,7 +31,5 @@ rotM(2,3) =  sin_3*sin_2*cos_1 - cos_3*sin_1;
 rotM(3,1) = -sin_2;
 rotM(3,2) =  cos_2*sin_1;
 rotM(3,3) =  cos_2*cos_1;
-
-
 
 end
